4f2f6095924a06b7dca71d7d213a5048cbb887cb
Merrick Mount <339474587@GoTVDSB.ca>
2024-04-23 14:24:08 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace loops_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userMin, userMax;
+            Random rand = new Random();
+            Console.WriteLine("This program is for randomization, it will give you 25 random numbers from lowest to highest given number");
+            Console.WriteLine("Please enter a minimum number");
+            userMin = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a maximum number");
+            userMax = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine(rand.Next(userMin, userMax));
+               
+            }
+        }
+    }
+}
